ef87faa6d93aff9c45f2d049c7e81595e8123896
Your Name <alex@heimstudio.ru>
2020-09-26 09:25:59 +0200

    lint corrections 7

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable import/no-named-as-default */
 
 import run from '../src/games/brain-even.js';
 
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/no-named-as-default-member */
 
 import run from '../src/games/brain-gcd.js';
 
